dfce615bcd15cb37f53a529be59f8f1b2b970764
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-10-11 22:02:26 +0000

    to do list with fecht finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
 	//Llamamos la Api con su usuario y la volvemos una promesa await
 	const getTask = async () => {
 		try {
-			let response = await fetch ("https://playground.4geeks.com/apis/fake/todos/user/josefrometa")
+			let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/josefrometa")
 
 			let data = await response.json()
 			console.log(data)
-			if (response.status == 200 ){
+			if (response.ok) {
 				setSaveTask(data)
 			}
 
@@ -27,34 +27,62 @@ const Home = () => {
 	}
 	//Guarda lo que escribimos en el input (Usamos un objeto que verificamos en el navegador, el objeto es el eveto a partir del evento buscamos el valor)
 	function handleTask(event) {
-		setTask(event.target.value)
+		setTask({
+			label: event.target.value,
+			done: false
+		})
 	}
 	//Funcion que agrega tareas a la lista
-	function handleTaskList(event) {
+	async function handleTaskList(event) {
 		if (event.key == "Enter") {
-			if (task.trim() !== "") {
-				setSaveTask([...saveTask, task.trim()])
-				setTask("");
+			if (task.label.trim() !== "") {
+				try {
+					let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/josefrometa", {
+						method: "PUT",
+						headers: { 
+							"Content-Type":"application/json"
+						 },
+						 body: JSON.stringify([...saveTask, task])
+					})
+					if (response.ok){
+						getTask()
+					}
+				} catch (error) {
+					console.log(error)
+				}
 			}
 		}
 	}
 	//Funcion que borra tareas de la lista (Realmente solo filtramos)
-	function handleDelete(index) {
+	async function handleDelete(index) {
 		let filterTask = saveTask.filter((item, indexFilter) => index !== indexFilter)
-		setSaveTask(filterTask)
+		try {
+			let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/josefrometa", {
+						method: "PUT",
+						headers: { 
+							"Content-Type":"application/json"
+						 },
+						 body: JSON.stringify(filterTask)
+					})
+					if (response.ok){
+						getTask()
+					}	
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
-	useEffect (() => {
+	useEffect(() => {
 		getTask()
 	}, [])
-		
+
 	return (
 
 		<div className="container d-flex flex-column justify-content-center">
 			<h1 className="text-center display-2 text-secondary">TO DO LIST</h1>
 			<div className="row justify-content-center">
 				<div className="col-6 text-center">
-					<input onChange={handleTask} value={task} onKeyDown={handleTaskList} className="w-100 rounded border-light fs-4 text-secondary " placeholder="Añadir Tarea" />
+					<input onChange={handleTask} value={task.label} onKeyDown={handleTaskList} className="w-100 rounded border-light fs-4 text-secondary " placeholder="Añadir Tarea" />
 				</div>
 			</div>
 			<div className="row justify-content-center">
@@ -64,7 +92,8 @@ const Home = () => {
 						{
 							saveTask.map((item, index) => {
 								return (<li className="border-bottom mt-2 ms-0  d-flex justify-content-between" key={index}>
-									<span className="text-secondary ">{item}</span> <i onClick={() => handleDelete(index)} className="fa-solid fa-trash  text-danger"></i> </li>)
+									<span className="text-secondary ">{item.label}</span>
+									<i onClick={() => handleDelete(index)} className="fa-solid fa-trash  text-danger"></i> </li>)
 							})
 						}
 					</ol>
